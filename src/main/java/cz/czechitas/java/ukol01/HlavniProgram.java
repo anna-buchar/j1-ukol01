8251bab9fe3b51abc2333ba1c33ebb33bbc17057
Anna <anickab99@gmail.com>
2025-04-19 19:21:30 +0200

    obrazky

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -12,7 +12,74 @@ public class HlavniProgram {
   public void start() {
     zofka = new Turtle();
 
-    //TODO implementace domácího úkolu
+    int delkaStrany = 80;
+
+    nakreslitPrasatko(delkaStrany);
+
+    //nakreslitSlunicko(20);
+
+
+
+  }
+
+  public void paprsek(int velikostKolecka){
+    int delkaPaprsku = velikostKolecka * 2;
+    zofka.turnLeft(100);
+    zofka.move(delkaPaprsku);
+    zofka.turnRight(180);
+    zofka.penUp();
+    zofka.move(delkaPaprsku);
+    zofka.turnLeft(80);
+    zofka.penDown();
+  }
+
+  public void nakreslitSlunicko(int velikostKolecka){
+    for (int i = 0; i < 12; i++) {
+        zofka.move(velikostKolecka);
+        zofka.turnRight(15);
+        zofka.move(velikostKolecka);
+        zofka.turnRight(15);
+        paprsek(velikostKolecka);
+    }
+  }
+
+  public void nakreslitDomecek(int stranaDomecku) {
+    for (int i = 0; i < 4; i++) {
+      zofka.move(stranaDomecku);
+      zofka.turnRight(90);
+    }
+    zofka.penUp();
+    zofka.move(stranaDomecku);
+    zofka.penDown();
+    zofka.turnRight(45);
+    zofka.move(stranaDomecku*(0.7));
+    zofka.turnRight(90);
+    zofka.move(stranaDomecku*(0.7));
+  }
+
+  public void nakreslitDveNozicky(int delkaNozicky) {
+    zofka.turnLeft(45);
+    zofka.penDown();
+    zofka.move(delkaNozicky);
+    zofka.penUp();
+    zofka.turnRight(135);
+    zofka.move(delkaNozicky*(1.5));
+    zofka.turnRight(135);
+    zofka.penDown();
+    zofka.move(delkaNozicky);
+  }
+
+  public void nakreslitPrasatko(int delkaStrany) {
+    int delkaNozicky = delkaStrany / 5;
+    zofka.turnLeft(90);
+    nakreslitDomecek(delkaStrany);
+    zofka.turnRight(135);
+    zofka.move(delkaStrany);
+    nakreslitDveNozicky(delkaNozicky);
+    zofka.turnRight(45);
+    zofka.move(delkaStrany);
+    zofka.turnRight(90);
+    nakreslitDveNozicky(delkaNozicky);
   }
 
 }
